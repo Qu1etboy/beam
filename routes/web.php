14b3f767e67b76f686e99dd2582f49f06811f1bb
Qu1etboy <nonzagreanthai@gmail.com>
2023-07-25 23:41:35 +0700

    feat(file): sample code on uploading file

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,5 +28,27 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+/**
+ * Sample on how to upload a file in laravel
+ */
+Route::post('/test/file', function(Request $request) {
+    
+    $request->validate([
+        'fileName' => ['required', 'string', 'max:255'],
+        'image' => ['required', 'file', 'image', 'max:2048']
+    ]);
+    
+    // NOTE: If not specified file name, laravel will generate for us.
+    // Specified file name and store in storage
+    $file = $request->file('image');
+    $path = $file->storeAs(
+        'public/images', $request->get('fileName') . '.' . $file->getClientOriginalExtension()
+    );
+ 
+    $filePath = str_replace('public/', '', $path);
+
+    return redirect(asset('storage/' . $filePath));
+})->name('test.file');
 
 require __DIR__.'/auth.php';
