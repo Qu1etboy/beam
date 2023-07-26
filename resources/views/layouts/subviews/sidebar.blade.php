b3977557ac7c7e8d5b7add97e71456613c2a9144
Qu1etboy <nonzagreanthai@gmail.com>
2023-07-27 03:22:57 +0700

    feat(ui): add sidebar

---
+++ resources/views/layouts/subviews/sidebar.blade.php
@@ -0,0 +1,10 @@
+<aside class="px-3 py-10 bg-white">
+  <h2 class="text-2xl px-2.5 font-bold">Event Name</h2>
+  <div class="space-y-2 py-4">
+    <a href="#" class="block hover:bg-gray-100 rounded-lg px-2.5 py-3">Dashboard</a>
+    <a href="#" class="block hover:bg-gray-100 rounded-lg px-2.5 py-3">Information</a>
+    <a href="#" class="block hover:bg-gray-100 rounded-lg px-2.5 py-3">Participants</a>
+    <a href="#" class="block hover:bg-gray-100 rounded-lg px-2.5 py-3">Tasks</a>
+    <a href="#" class="block hover:bg-gray-100 rounded-lg px-2.5 py-3">Financial</a>
+  </div>
+</aside>
---
+++ resources/views/organizer/event/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.organizer')
+
+@section('sub-content')
+<div class="p-3">
+    <h1 class="font-bold text-4xl my-3">Event Dashboard</h1>
+</div>
+@endsection
