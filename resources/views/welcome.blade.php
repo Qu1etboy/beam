41e5f1789a919d4196db483f4602f19b4f0ce05c
Qu1etboy <nonzagreanthai@gmail.com>
2023-07-27 02:45:43 +0700

    feat(ui): create main layout and home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,51 +1,31 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.main')
 
-        <title>Laravel</title>
+@section('content')
+<div class="container mx-auto p-3">
+    <h1 class="font-bold text-4xl my-3">All Events</h1>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <section class="my-8 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-3 gap-y-6 place-items-center">
 
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="antialiased">
-        <main>
-            <div>
-                @auth
-                    <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
-                @else
-                    <a href="{{ url('/auth/google') }}" class="text-white bg-black hover:bg-black/90 focus:ring-4 focus:outline-none focus:ring-gray-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2">
-                        <svg class="w-4 h-4 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
-                            <path fill-rule="evenodd" d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z" clip-rule="evenodd"/>
-                        </svg>
-                        Sign in with Google
-                    </a>
-                @endauth
-            </div>
-            
-            <form action="{{ route('test.file') }}" method="POST" enctype="multipart/form-data" class="px-3 space-y-2">
-                @csrf
-                <div>
-                    <label for="fileName" class="block mb-2 text-sm font-medium text-gray-900">File name</label>
-                    <input type="text" id="fileName" name="fileName" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5" placeholder="Enter your file name" required>
+        @for($i = 0; $i < 10; $i++)
+            <!-- Event card-->
+            <a href="/event" class="max-w-lg">
+                <img 
+                    src="https://images.unsplash.com/photo-1690221129223-e5a996041fec?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80" 
+                    alt="poster"
+                    class="rounded-lg object-cover h-[300px]"    
+                >
+                <div class="mt-3">
+                    <span class="text-sm text-red-500">Sat, 05 Aug</span>
+                    <h2 class="font-semibold">KU First Meet</h2>
+                    <p class="text-gray-600 text-sm">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pin inline-block"><path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z"/><circle cx="12" cy="10" r="3"/></svg>
+                        Location
+                    </p>
                 </div>
-                <div>
-                    <label class="block mb-2 text-sm font-medium text-gray-900" for="file_input">Upload file</label>
-                    <input type="file" name="image" id="file_input" accept="image/*" 
-                        class="block w-full border border-gray-200 shadow-sm rounded-md text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500
-                        file:bg-transparent file:border-0
-                        file:bg-black file:text-white file:mr-4
-                        file:py-3 file:px-4" 
-                        required
-                    >
-                    <p class="mt-1 text-sm text-gray-500" id="file_input_help">SVG, PNG, JPG or GIF.</p>
-                </div>
-                <button type="submit" class="text-white bg-black hover:bg-black/90 focus:ring-4 focus:outline-none focus:ring-gray-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2">Upload</button>
-            </form>
-        </main>
-    </body>
-</html>
+            </a>    
+        @endfor
+
+    </section>
+
+</div>
+@endsection
