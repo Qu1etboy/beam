22f596e9d96c88c960c7de00bb9b00611128172d
ong22280 <ong22280@gmail.com>
2023-07-28 15:15:55 +0700

    implement: All Models

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -8,4 +8,38 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'organizer_id',
+        'event_name',
+        'event_description',
+        'poster_image',
+        'location',
+        'registrant_questions',
+    ];
+
+    public function organizer()
+    {
+        return $this->belongsTo(Organizer::class, 'organizer_id');
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class, 'event_id');
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class, 'event_id');
+    }
+
+    public function registrantQuestions()
+    {
+        return $this->hasMany(RegistrantQuestion::class, 'event_id');
+    }
+
+    public function participants()
+    {
+        return $this->belongsToMany(User::class, 'event_user', 'event_id', 'user_id');
+    }
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'detail',
+        'cost',
+        'event_id',
+    ];
+
+    public function event()
+    {
+        return $this->belongsTo(Event::class, 'event_id');
+    }
 }
