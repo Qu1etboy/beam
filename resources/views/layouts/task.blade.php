dbd79d225a478daaf0854c8510abfcefdf99e70a
Qu1etboy <nonzagreanthai@gmail.com>
2023-08-01 22:15:09 +0700

    feat(ui): responsive kanban board

--- resources/views/layouts/task.blade.php
+++ resources/views/layouts/task.blade.php
@@ -9,8 +9,8 @@
 
         <nav class="flex items-center justify-between gap-5 border rounded-lg w-full p-3 mb-8">
           <div class="space-x-5">
-            <a href="{{ route('organizer.event.tasks.list') }}" class="hover:text-emerald-500 duration:300">List</a>
-            <a href="{{ route('organizer.event.tasks.board') }}" class="hover:text-emerald-500 duration:300">Board</a>
+            <a href="{{ route('organizer.event.tasks.list') }}" class="hover:text-purple-500 duration:300">List</a>
+            <a href="{{ route('organizer.event.tasks.board') }}" class="hover:text-purple-500 duration:300">Board</a>
           </div>
           <a href="{{ route('organizer.event.tasks.add') }}" class="text-white bg-black hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Add Task</a>
         </nav>
