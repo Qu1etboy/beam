6aedbac6eddf3b051372571c128503e2a292fb35
Qu1etboy <nonzagreanthai@gmail.com>
2023-08-01 22:23:44 +0700

    fix(ui): change to textarea

--- resources/views/organizer/event/information.blade.php
+++ resources/views/organizer/event/information.blade.php
@@ -19,7 +19,7 @@
 
           <div class="mb-6">
             <label for="description" class="block mb-2 text-sm font-medium text-gray-900">Event description</label>
-            <input type="text" id="description" placeholder="Enter your event description" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+            <textarea type="text" id="description" placeholder="Enter your event description" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"></textarea>
           </div>
 
           <div class="mb-6">
