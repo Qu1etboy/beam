3cdcfd59053a54537b001e07258310e29e2054c2
Qu1etboy <nonzagreanthai@gmail.com>
2023-08-01 23:51:25 +0700

    feat(ui): add rich text editor

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -14,5 +14,13 @@
     </head>
     <body>
       @yield('body')
+
+      <!-- CK Editor -->
+      <script src="//cdn.ckeditor.com/4.14.1/standard/ckeditor.js"></script>
+      <script type="text/javascript">
+        $(document).ready(function () {
+            $('.ckeditor').ckeditor();
+        });
+      </script>
     </body>
 </html>
