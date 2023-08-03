030a23bb0b9dab63df91809ce7d3b0faa261e390
Qu1etboy <nonzagreanthai@gmail.com>
2023-08-03 15:35:10 +0700

    fix(ui): show hero section only on first page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,7 @@
 
 @section('content')
 <div>
+    @if ($events->currentPage() === 1)
     <div class="relative flex flex-col justify-center h-[600px] bg-cover bg-center bg-[url('https://images.unsplash.com/photo-1450849608880-6f787542c88a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2842&q=80')]">
         <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full flex flex-col justify-center items-center bg-black/50 backdrop-blur-md"></div>
         <div class="container mx-auto z-[2] px-3">
@@ -11,6 +12,7 @@
             <p class="text-lg md:text-xl mx-1 text-gray-100">Over 100+ events right here for you</p>
         </div>
     </div>
+    @endif
 
     <section class="container mx-auto p-3 mt-5">
         <h1 class="font-bold text-4xl my-3">All Events</h1>
