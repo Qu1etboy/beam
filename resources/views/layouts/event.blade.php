14fd28f3a495a71ab925759631eb785e41efa295
Qu1etboy <nonzagreanthai@gmail.com>
2023-08-08 19:58:18 +0700

    fix(ui): make sidebar position fixed

--- resources/views/layouts/event.blade.php
+++ resources/views/layouts/event.blade.php
@@ -2,9 +2,9 @@
 
 @section('body')
     <main>
-        <div class="grid lg:grid-cols-5 w-full">
+        <div class="grid lg:grid-cols-5 w-full h-screen">
             @include('layouts.subviews.sidebar')
-            <div class="col-span-4">
+            <div class="col-span-4 overflow-y-auto">
                 @include('layouts.subviews.organizer-navbar', ['organizer' => $organizer])
                 <div class="p-4">
                     @yield('sub-content')
