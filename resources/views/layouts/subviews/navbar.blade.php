a0aea4e8e7133ea0b9cc4532c4ac41a7fbaecc1a
Qu1etboy <nonzagreanthai@gmail.com>
2023-08-08 20:50:01 +0700

    feat(home): responsive main site navbar

--- resources/views/layouts/subviews/navbar.blade.php
+++ resources/views/layouts/subviews/navbar.blade.php
@@ -1,8 +1,76 @@
 <header class="p-3 border-b">
-  <nav class="container mx-auto flex justify-between items-center">
-    <div class="flex gap-5">
-      <a href="{{ route('index') }}" class="text-3xl font-bold">Beam</a>
-      <form>
+    <nav class="container mx-auto flex justify-between items-center">
+        <div class="flex gap-5">
+            <a href="{{ route('index') }}" class="text-3xl font-bold">Beam</a>
+            
+            <!-- Desktop search input -->
+            <form class="hidden md:block w-80">
+                <label for="simple-search" class="sr-only">Search</label>
+                <div class="relative w-full">
+                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                    </svg>
+                </div>
+                <x-text-input type="search" id="simple-search" placeholder="Search event name" class="pl-10" />
+                </div>
+            </form>
+        </div>
+
+        <div class="flex items-center gap-3">
+        
+            @auth
+                <a href="{{ route('organizer.home') }}" class="hidden md:block hover:text-purple-500 duration-300">Organization</a>        
+                <div>
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                {{-- use default Avatar --}}
+                                <x-user-avatar :profile_url="Auth::user()->avatar" class="w-10 h-10" />
+                            </button>
+                        </x-slot>
+
+                        <x-slot name="content">
+                            <h2 class="text-sm px-4 py-3">{{ Auth::user()->name }}</h2>
+                            <x-dropdown-link :href="route('organizer.home')">
+                                {{ __('My Organization') }}
+                            </x-dropdown-link>
+                            
+                            <x-dropdown-link :href="route('orders')">
+                                {{ __('My Orders') }}
+                            </x-dropdown-link>
+
+                            <x-dropdown-link :href="route('settings')">
+                                {{ __('Settings') }}
+                            </x-dropdown-link>
+
+                            <!-- Authentication -->
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+
+                                <x-dropdown-link :href="route('logout')"
+                                        onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                                    {{ __('Log Out') }}
+                                </x-dropdown-link>
+                            </form>
+                        </x-slot>
+                    </x-dropdown>
+                </div>
+
+            @else
+                <a href="{{ url('/auth/google') }}" class="text-white bg-black hover:bg-black/90 focus:ring-4 focus:outline-none focus:ring-gray-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2">
+                    <svg class="w-4 h-4 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
+                        <path fill-rule="evenodd" d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z" clip-rule="evenodd"/>
+                    </svg>
+                    Sign in with Google
+                </a>
+            @endauth
+        </div>
+    </nav>
+
+    <!-- Mobile search input -->
+    <form class="w-full container mx-auto md:hidden block mt-2">
         <label for="simple-search" class="sr-only">Search</label>
         <div class="relative w-full">
           <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -10,61 +78,9 @@
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
               </svg>
           </div>
-          <x-text-input id="simple-search" placeholder="Search event name" class="pl-10" />
+          <x-text-input type="search" id="simple-search" placeholder="Search event name" class="pl-10" />
         </div>
-      </form>
-    </div>
+    </form>
 
-    @auth
-        <div class="flex items-center gap-3">
-          <a href="{{ route('organizer.home') }}" class="hover:text-purple-500 duration-300">Organization</a>        
-          <div>
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                            {{-- use default Avatar --}}
-                            <x-user-avatar :profile_url="Auth::user()->avatar" class="w-10 h-10" />
-                        </button>
-                    </x-slot>
 
-                    <x-slot name="content">
-                        <h2 class="text-sm px-4 py-3">{{ Auth::user()->name }}</h2>
-                        {{-- <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
-                        </x-dropdown-link> --}}
-
-                        <x-dropdown-link :href="route('orders')">
-                            {{ __('My Orders') }}
-                        </x-dropdown-link>
-
-                        <x-dropdown-link :href="route('settings')">
-                            {{ __('Settings') }}
-                        </x-dropdown-link>
-
-                        <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </form>
-                    </x-slot>
-                </x-dropdown>
-            </div>
-        
-        
-        </div>
-    @else
-        <a href="{{ url('/auth/google') }}" class="text-white bg-black hover:bg-black/90 focus:ring-4 focus:outline-none focus:ring-gray-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2">
-            <svg class="w-4 h-4 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
-                <path fill-rule="evenodd" d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z" clip-rule="evenodd"/>
-            </svg>
-            Sign in with Google
-        </a>
-    @endauth
-
-  </nav>
 </header>
