f0a3903d9e5ddca9d1a14a5f5059afc895f84bfa
Qu1etboy <nonzagreanthai@gmail.com>
2023-08-15 23:56:03 +0700

    feat(avatar): display default avatar

--- resources/views/components/user-avatar.blade.php
+++ resources/views/components/user-avatar.blade.php
@@ -2,4 +2,9 @@
   'profile_url'
 ])
 
-<img src="{{ file_exists('storage/' . $profile_url) ? asset('storage/' . $profile_url) : $profile_url }}" alt="user avatar" {!! $attributes->merge(['class' => 'rounded-full object-cover']) !!}>
+<img 
+  src="{{ file_exists('storage/' . $profile_url) ? asset('storage/' . $profile_url) : $profile_url }}" 
+  alt="user avatar" 
+  onerror="this.src='https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541'"
+  {!! $attributes->merge(['class' => 'rounded-full object-cover']) !!}
+>
