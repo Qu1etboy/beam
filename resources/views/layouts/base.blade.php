d810cbf00f3de68539efe35e85f33ff0e27d996e
Qu1etboy <nonzagreanthai@gmail.com>
2023-08-19 06:40:45 +0700

    chore: add favicon

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>@yield('title')</title>
 
+        <link rel="icon" type="image/x-icon" href="/favicon.ico">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
