51bd210e374b3fcba2383f3bd62813dd84463f8c
Qu1etboy <nonzagreanthai@gmail.com>
2023-08-18 17:47:37 +0700

    feat: add confirmation model before delete item

--- resources/views/organizer/event/financial.blade.php
+++ resources/views/organizer/event/financial.blade.php
@@ -64,7 +64,7 @@
                 <form action="{{ route('orders.destroy', ['order' => $order, 'organizer' => $organizer, 'event' => $event]) }}" method="POST">
                   @csrf
                   @method('delete')
-                  <button type="submit" class="text-red-600 flex items-center gap-1 hover:bg-gray-100 rounded-md duration-300 p-1">
+                  <button type="submit" onclick="return confirm('Are you sure you want to delete this order?');" class="text-red-600 flex items-center gap-1 hover:bg-gray-100 rounded-md duration-300 p-1">
                     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-2"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/><line x1="10" x2="10" y1="11" y2="17"/><line x1="14" x2="14" y1="11" y2="17"/></svg>
                     <span>Delete</span>
                   </button>
